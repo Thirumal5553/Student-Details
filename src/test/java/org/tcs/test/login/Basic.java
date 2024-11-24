744c3d7097a4789eff82e5c73be70042bcd71ba3
Thirumal5553 <thriumal5553@gmail.com>
2024-11-24 11:14:15 +0530

    Sample Data

---
+++ src/test/java/org/tcs/test/login/Basic.java
@@ -0,0 +1,18 @@
+package org.tcs.test.login;
+
+public class Basic {
+	public void studentName() {
+		System.out.println("Thirumal");
+
+	}
+	
+	public void studentAge() {
+		System.out.println("29");
+	}
+
+	public static void main(String[] args) {
+		Basic t = new Basic();
+		t.studentName();
+		t.studentAge();
+	}
+}
